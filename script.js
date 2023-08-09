2bc4034564d5a86dd81745092baafabe75ad0890
Eduardo Puente Castillo <puente@Eduardos-MBP.attlocal.net>
2023-08-08 19:11:59 -0700

    finished project

--- script.js
+++ script.js
@@ -9,12 +9,21 @@ btn.addEventListener("click", copyText);
 // functions
 function copyText(e) {
   e.preventDefault();
-  coupon.select();
-  coupon.setSelectionRange(0, -1);
-  document.execCommand("copy");
-  btn.innerHTML = "Copied!";
 
-  setTimeout(() => {
-    btn.innerHTML = "Copy";
-  }, 2000);
+  //? deprecated
+  // coupon.select();
+  // coupon.setSelectionRange(0, -1);
+  // document.execCommand("copy");
+  // btn.innerHTML = "Copied!";
+  // setTimeout(() => {
+  //   btn.innerHTML = "Copy";
+  // }, 2000);
+
+  //? new way
+  navigator.clipboard.writeText(coupon.value).then(() => {
+    btn.innerHTML = "Copied!";
+    setTimeout(() => {
+      btn.innerHTML = "Copy";
+    }, 2000);
+  });
 }
